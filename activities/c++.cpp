327399b664cfebc957ab70a6676c331cb90c40f8
Rhafael Cabral <rhafaelcabral.rodrigues@gmail.com>
2026-03-11 23:21:02 -0300

    ADDED: Atividade 2 - Criando primeiro repositório local e no github

---
+++ activities/c++.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double temperatura = 26.5;
+    cout << "Sensor ativo: " << temperatura << " C" << endl;
+    return 0;
+}
---
+++ activities/python.py
@@ -0,0 +1,6 @@
+def main():
+    temperatura = 26.5
+    print(f"Sensor ativo: {temperatura} C")
+
+if __name__ == "__main__":
+    main()
